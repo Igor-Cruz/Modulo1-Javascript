d264ad48ce6f4b0ac069a12fd9657ae7a9156f46
IGOR CRUZ <igorcruz.it@outlook.com>
2021-11-29 17:14:50 -0300

    Aprofundando sobre os modos de variáveis

---
+++ variaveis.js
@@ -0,0 +1,51 @@
+// Var forma mais antiga e mais perigosa de usar 
+
+
+var a = 10
+let b = 20
+const c = 30
+
+const d = {
+  value: 50
+}
+d.value = 40
+
+// Tipos de Variáveis
+//Float
+b = 1.0
+
+//String - Pode ser declarado tanto com aspas duplas ou simples
+let name = 'Igor Cruz'
+
+//Objeto Geralmente um key e um value
+
+let obj = {
+  cidade: 'Lauro de freitas',
+  Bairro: 'Ipitanga',
+  insp: {
+    CEP: 42706020
+  }
+
+}
+/* Acessar obj*/
+obj.cidade
+obj.insp.CEP
+
+let now = obj.insp.CEP
+let other = obj['insp']['CEP']
+console.log(other)
+
+//Vetor
+
+let vetor = [1, 2, 3, 4, 5]
+console.log(vetor[2])
+
+//Função em uma variável
+
+let funcao = function () {
+  console.log('de dentro da função')
+}
+let funcaoC = funcao
+funcaoC()
+
+
